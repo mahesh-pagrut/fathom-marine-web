8e893cf27ffa779adc8f301b61a61794afcb791b
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-05-24 11:13:26 +0530

    code web structure updated bugs fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
 
 function App() {
-
   return (
-    <div className="text-sm text-blue-600 font-bold ">Hello Tailwind!</div>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
 
-  )
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+        
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
