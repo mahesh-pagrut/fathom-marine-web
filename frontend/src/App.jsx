c680d8bb3e06848740d1339eaf45079607d2a1ce
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-05-24 12:38:57 +0530

    fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
+// import { Routes, Route } from 'react-router-dom';
+// import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-
-      <main className="flex-grow">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-        
-        </Routes>
-      </main>
-
-      <Footer />
+    <div>
+      mahesh
     </div>
   );
 }
